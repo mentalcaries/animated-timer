de1dd37ecce944df98221c4e51686439d8465aa8
Devin Jaggernauth <neexis@gmail.com>
2021-10-12 06:26:13 -0400

    add tick method

--- components/Timer.js
+++ components/Timer.js
@@ -4,13 +4,20 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener('click', this._start)
+    this.startButton.addEventListener('click', this._start);
+    this.pauseButton.addEventListener('click', this._pause)
   }
-  _start(){
-    console.log('time to start')
-  }
+  _start = () => {
+    this._tick;
+    this.interval = setInterval(this._tick, 1000);
+  };
+  _tick = () => {
+    const timeRemaining = parseFloat(this.durationInput.value)
+  };
 
-
+  _pause = () => {
+    clearInterval(this.interval);
+  };
 }
 
 export default Timer;
